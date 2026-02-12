ea278f7842f78783ae6e7468d528cafabbddef58
Hoshieko <Jujocamcas22@gmail.com>
2026-02-12 17:59:56 -0500

    Feat: Se ha modificado la Entity de CulturalProject para adaptarla al diagrama de la base de datos, asi como se han añadido columns a los atributos con el fin de no generar conflictos

--- src/main/java/com/example/colectivoIkuna/domain/model/CulturalProject.java
+++ src/main/java/com/example/colectivoIkuna/domain/model/CulturalProject.java
@@ -5,32 +5,51 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 @Entity
-@Table(name = "ikuna_projects")
+@Table(name = "Proyecto")
 @Data
 public class CulturalProject {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "IdProyecto")
   private Long id;
 
+  @Column(name = "TituloProyecto")
   private String title;
-  private String category; // 'Espacios', 'Eventos', etc.
-  private String status;   // 'completed', 'in-progress'
+  @Column(name = "LineaTrabajoCultural")
+  private String category;
+  @Column(name = "EstadoProyecto")
+  private String status;
+  @Column(name = "ProgresoEjecucionProyecto")
   private Integer progress;
-  private LocalDate executionDate; // En BD es fecha real
+  @Column(name = "FechaInicioProyecto")
+  private LocalDate executionDate;
+  @Column(name = "MontoInicial")
+  private BigDecimal totalBudget;
+    @Column(name = "MontoEjecutado")
+  private BigDecimal executedBudget;
 
-  @Column(columnDefinition = "TEXT")
+
+  @Column(name = "DescripcionProyecto", columnDefinition = "TEXT")
   private String description;
+  @Column(name = "ImagenReferencia")
   private String coverImageUrl;
 
   // Relaciones: Un proyecto tiene muchos miembros y tareas
   // CascadeType.ALL significa que si guardas el proyecto, se guardan las tareas automáticamente
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn(name = "project_id")
+  @JoinColumn(name = "IdProyecto")
   private List<TeamMember> teamMembers = new ArrayList<>();
 
   @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-  @JoinColumn(name = "project_id")
+  @JoinColumn(name = "IdProyecto")
   private List<Task> tasks = new ArrayList<>();
+
+  @OneToMany(mappedBy = "proyecto", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<EventCalendar> eventCalendars = new ArrayList<>();
+
+  @OneToMany(mappedBy = "proyecto", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<ProjectCollaboration> colaboradores = new ArrayList<>();
 }
